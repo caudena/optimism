runtimeverification/_audits_Ethereum-optimism_optimism_interopv2

feat: add scaffolding for making kona the default respected game type

packages/contracts-bedrock/src/libraries/DevFeatures.sol
@@ -28,6 +28,11 @@ library DevFeatures {
     /// @notice The feature that enables the OPContractsManagerV2 contract.
     bytes32 public constant OPCM_V2 = bytes32(0x0000000000000000000000000000000000000000000000000000000000010000);
 
+    /// @notice The feature that makes CANNON_KONA the respected game type instead of CANNON.
+    /// @dev Not to be confused with the legacy CANNON_KONA flag (nibble 1) which controlled game deployment.
+    bytes32 public constant RESPECTED_GAME_TYPE_CANNON_KONA =
+        bytes32(0x0000000000000000000000000000000000000000000000000000000000100000);
+
     /// @notice Checks if a feature is enabled in a bitmap. Note that this function does not check
     ///         that the input feature represents a single feature and the bitwise AND operation
     ///         allows for multiple features to be enabled at once. Users should generally check

caudena/optimism

packages/contracts-bedrock/src/libraries/DevFeatures.sol
@@ -31,6 +31,11 @@ library DevFeatures {
     /// @notice The feature that enables L2CM.
     bytes32 public constant L2CM = bytes32(0x0000000000000000000000000000000000000000000000000000000000100000);
 
+    /// @notice The feature that makes CANNON_KONA the respected game type instead of CANNON.
+    /// @dev Not to be confused with the legacy CANNON_KONA flag (nibble 1) which controlled game deployment.
+    bytes32 public constant RESPECTED_GAME_TYPE_CANNON_KONA =
+        bytes32(0x0000000000000000000000000000000000000000000000000000000001000000);
+
     /// @notice Checks if a feature is enabled in a bitmap. Note that this function does not check
     ///         that the input feature represents a single feature and the bitwise AND operation
     ///         allows for multiple features to be enabled at once. Users should generally check
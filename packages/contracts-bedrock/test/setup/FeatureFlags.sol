1f9970450eaffe2ed6c1f0f09bffe21d7317c93c
steven <steven.nevins.js@gmail.com>
2026-02-09 15:50:54 -0500

    feat: add scaffolding for making kona the default respected game type

--- packages/contracts-bedrock/test/setup/FeatureFlags.sol
+++ packages/contracts-bedrock/test/setup/FeatureFlags.sol
@@ -49,6 +49,10 @@ abstract contract FeatureFlags {
             console.log("Setup: DEV_FEATURE__L2CM is enabled");
             devFeatureBitmap |= DevFeatures.L2CM;
         }
+        if (Config.devFeatureRespectedGameTypeCannonKona()) {
+            console.log("Setup: DEV_FEATURE__RESPECTED_GAME_TYPE_CANNON_KONA is enabled");
+            devFeatureBitmap |= DevFeatures.RESPECTED_GAME_TYPE_CANNON_KONA;
+        }
     }
 
     /// @notice Returns the string name of a feature.
@@ -61,6 +65,8 @@ abstract contract FeatureFlags {
             return "DEV_FEATURE__OPCM_V2";
         } else if (_feature == DevFeatures.L2CM) {
             return "DEV_FEATURE__L2CM";
+        } else if (_feature == DevFeatures.RESPECTED_GAME_TYPE_CANNON_KONA) {
+            return "DEV_FEATURE__RESPECTED_GAME_TYPE_CANNON_KONA";
         } else if (_feature == Features.CUSTOM_GAS_TOKEN) {
             return "SYS_FEATURE__CUSTOM_GAS_TOKEN";
         } else if (_feature == Features.ETH_LOCKBOX) {
